efbaf012d26d4dcf8cd906f7700eaaaacd46d3bd
Max Dickens <mtdickens1998@gmail.com>
2026-02-02 22:52:25 +0800

    Refactor imports in ExtendedCreasePattern and MainWindow for clarity and organization

--- src/main/java/ovgu/creasy/origami/ExtendedCreasePattern.java
+++ src/main/java/ovgu/creasy/origami/ExtendedCreasePattern.java
@@ -1,5 +1,15 @@
 package ovgu.creasy.origami;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import oripa.domain.creasepattern.CreasePatternInterface;
 import oripa.domain.fold.foldability.FoldabilityChecker;
 import oripa.domain.fold.halfedge.OrigamiModel;
@@ -10,29 +20,29 @@ import ovgu.creasy.origami.basic.CreasePattern;
 import ovgu.creasy.origami.basic.DiagramStep;
 import ovgu.creasy.origami.oripa.OripaTypeConverter;
 
-import java.util.*;
-import java.util.stream.Collectors;
-
 /**
- * Extended Crease Pattern, adds information about Reflection Creases
- * to a crease Pattern and is used for the step generation
- * Explained in chapter 4.2 of the paper (pages 28 - 36)
+ * Extended Crease Pattern, adds information about Reflection Creases to a
+ * crease Pattern and is used for the step generation Explained in chapter 4.2
+ * of the paper (pages 28 - 36)
  */
 public class ExtendedCreasePattern {
+
     private Set<Vertex> vertices;
     private Set<ExtendedCrease> creases;
     private Map<Vertex, List<ExtendedCrease>> connections;
     private List<DiagramStep> possibleSteps;
     private CreasePattern cp;
+
     /**
      * @param vertices is the set of extended vertices
      * @param creases is the set of directed edges of the extended graph.
-     * @param connections is a set of ordered, circular lists of edges parting from each vertex in vertices
+     * @param connections is a set of ordered, circular lists of edges parting
+     * from each vertex in vertices
      */
     public ExtendedCreasePattern(Set<Vertex> vertices,
-                                 Set<ExtendedCrease> creases,
-                                 Map<Vertex, List<ExtendedCrease>> connections,
-                                 CreasePattern cp) {
+            Set<ExtendedCrease> creases,
+            Map<Vertex, List<ExtendedCrease>> connections,
+            CreasePattern cp) {
         this.vertices = vertices;
         this.creases = creases;
         this.connections = connections;
@@ -51,13 +61,15 @@ public class ExtendedCreasePattern {
         return Collections.unmodifiableSet(creases);
     }
 
-    public Map<Vertex, List<ExtendedCrease>> getAdjacencyLists() { return Collections.unmodifiableMap(connections); }
+    public Map<Vertex, List<ExtendedCrease>> getAdjacencyLists() {
+        return Collections.unmodifiableMap(connections);
+    }
 
     @Override
     public String toString() {
-        return "ExtendedCreasePattern{" +
-            "xC=" + creases +
-            '}';
+        return "ExtendedCreasePattern{"
+                + "xC=" + creases
+                + '}';
     }
 
     public static List<ExtendedCreasePattern> createECPs(CreasePattern cp, boolean randomized) {
@@ -141,10 +153,9 @@ public class ExtendedCreasePattern {
         return steps;
     }
 
-
     private Set<ExtendedReflectionPath> findSimpleFolds(Vertex vertex) {
         List<ExtendedCrease> outgoing = this.connections.get(vertex);
-        return  outgoing.stream()
+        return outgoing.stream()
                 .filter(crease -> crease.getType() != Crease.Type.EDGE)
                 .filter(ExtendedCrease::isComplete)
                 .map(ExtendedCrease::getExtendedReflectionPath).collect(Collectors.toSet());
